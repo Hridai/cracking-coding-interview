b31021e3b107bea769429fff562e8ab1c5680964
Hridai Trivedy <hridaitrivedy@gmail.com>
2026-02-20 10:59:59 +1100

    python chapter 1

---
+++ coding_solution_python/chapter1/3-URLify.py
@@ -0,0 +1,23 @@
+"""
+Problem:
+Write a method to replace all spaces in a string with '%20'. You may assume that the string has 
+sufficient space at the end to hold the additional characters, and that you are given the "true"
+length of the string.
+"""
+
+"""
+Questions:
+
+
+Algorithm:
+
+
+"""
+
+
+def test_func():
+    return True
+
+
+if __name__ == "__main__:
+    test_func()
---
+++ coding_solution_python/solution_template.py
@@ -0,0 +1,21 @@
+"""
+Problem:
+
+"""
+
+"""
+Questions:
+
+
+Algorithm:
+
+
+"""
+
+
+def test_func():
+    return True
+
+
+if __name__ == "__main__:
+    test_func()
